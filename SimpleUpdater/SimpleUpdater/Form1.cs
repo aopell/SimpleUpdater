e337888a0345dcad80a9170807225706ce2de30f
Aaron Opell <aaron.opell@live.com>
2016-05-30 12:44:58 -0700

    Compatibility commit

---
+++ SimpleUpdater/SimpleUpdater/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimpleUpdater
+{
+    public partial class Form1 : Form
+    {
+        string[] args;
+        string temp;
+        public Form1(string[] inArgs)
+        {
+            args = inArgs;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string downloadUrl = args[0];
+                label2.Text = "Downloading from " + downloadUrl;
+                WebClient wc = new WebClient();
+                wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                temp = Path.GetTempFileName();
+                wc.DownloadFile(downloadUrl, temp);
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("The update failed. Please try again later.", "Update Failed", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+            }
+        }
+
+        void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            File.Move(temp, args[1]);
+            Process.Start(args[1]);
+            Application.Exit();
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            label1.Text = e.ProgressPercentage.ToString();
+            progressBar1.Value = e.ProgressPercentage;
+        }
+    }
+}
---
+++ SimpleUpdater/SimpleUpdater/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Form2(args)).ShowDialog();
+        }
+    }
+}
