a9521e631d881a8320729e4ac0e4c78b45c2bc51
Aaron Opell <aaron.opell@live.com>
2016-10-08 15:42:51 -0700

    Added Zip Updater

---
+++ SimpleUpdater/SimpleZipUpdater/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SimpleZipUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Waiting to Start";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Downloading";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(490, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0%";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 46);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(469, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 107);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ SimpleUpdater/SimpleZipUpdater/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+namespace SimpleZipUpdater
+{
+    public partial class Form1 : Form
+    {
+        string[] Args;
+        string temp;
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            Args = args;
+        }
+
+        /*
+         * Args[0] = Download Url
+         * Args[1] = Unzip Path
+         * Args[2] = Executable Name / Relative Path 
+         */
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string downloadUrl = Args[0];
+                label2.Text = "Downloading from " + downloadUrl;
+                WebClient wc = new WebClient();
+                wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+
+                wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+                temp = Path.GetTempFileName();
+                wc.DownloadFileAsync(new Uri(downloadUrl), temp);
+                label3.Text = "Download in progress";
+            }
+            catch (Exception ex)
+            {
+                label3.Text = "Download failed";
+                MessageBox.Show("The download failed. Please try again later.", "Download Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            label1.Text = e.ProgressPercentage + "%";
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                label3.Text = "Download completed - Extracting Files";
+
+                if (!Directory.Exists(Args[1]))
+                    Directory.CreateDirectory(Args[1]);
+
+                ZipArchive zip = new ZipArchive(new FileStream(temp, FileMode.Open));
+
+                zip.ExtractToDirectory(Args[1], true);
+                Process.Start(Path.Combine(Args[1], Args[2]));
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                label3.Text = "Update failed";
+                MessageBox.Show("The update failed. Please try again later.", "Update Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+    }
+
+    public static class ZipArchiveExtensions
+    {
+        public static void ExtractToDirectory(this ZipArchive archive, string destinationDirectoryName, bool overwrite)
+        {
+            if (!overwrite)
+            {
+                archive.ExtractToDirectory(destinationDirectoryName);
+                return;
+            }
+            foreach (ZipArchiveEntry file in archive.Entries)
+            {
+                string completeFileName = Path.Combine(destinationDirectoryName, file.FullName);
+
+                if (!Directory.Exists(Path.GetDirectoryName(completeFileName)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+
+                if (file.Name == "")
+                {// Assuming Empty for Directory
+                    Directory.CreateDirectory(Path.GetDirectoryName(completeFileName));
+                    continue;
+                }
+                file.ExtractToFile(completeFileName, true);
+            }
+        }
+    }
+}
